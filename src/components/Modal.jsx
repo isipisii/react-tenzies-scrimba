cd3ee26e0fa71e35d55a9002ee7201c575d6bff4
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-01-18 17:50:17 +0800

    fixed the time

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './Modal.css'
 
 function Modal(props) {
-
+  const finalTime = props.time
   return (
     <div className='modal'>
         <div className='modal-text'>
             <h1 className='congrats-text'>Congratulations, you've completed the dices!</h1>
-            <p>You have completed the dices with <b>{props.attempt}</b> rolls and within <b>{props.time}</b> seconds.</p>
+            <p>You have completed the dices with <b>{props.attempt}</b> rolls and within <b>{finalTime}</b> seconds.</p>
         </div>
         <button className='new-game-btn' onClick={props.newGame}>New Game</button>
     </div>
